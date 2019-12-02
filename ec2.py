c9c7fb8da43778bea400b2ba2c95a487f279bff5
QingduMeng <58366690+QingduMeng@users.noreply.github.com>
2019-12-01 22:39:43 -0500

    Update ec2.py

--- ec2.py
+++ ec2.py
@@ -7,4 +7,5 @@ ec2.create_instances(ImageId='ami-00068cd7555f543d5', MinCount=1, MaxCount=1)
 # terminate an instance
 ids = ['i-04d55ec0552e0bb40']
 ec2.instances.filter(InstanceIds=ids).terminate()
+
 print('done')
